5bce307fcfeead8a0daba920b6b6b4b775d19502
ProphetMalzahar <61146042+ProphetMalzahar@users.noreply.github.com>
2020-08-22 15:45:32 +0800

    Update 四数之和.java

--- 四数之和/四数之和.java
+++ 四数之和/四数之和.java
@@ -1,1 +1,32 @@
-
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        //结果列表
+        List<List<Integer>> lists=new ArrayList<>();
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i==0||i>0&&nums[i]!=nums[i-1])
+            {
+                int l = i + 1,r=nums.length-1,sum=0-nums[i];
+                while (l<r)
+                {
+                    if (nums[l]+nums[r]==sum)
+                    {
+                        lists.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                        while (l<r&&nums[l]==nums[l+1]) l++;
+                        while (l<r&&nums[r]==nums[r-1]) r--;
+                        l++;
+                        r--;
+                    }else if (nums[l]+nums[r]<sum)
+                    {
+                        while (l<r&&nums[l]==nums[l+1]) l++;
+                        l++;
+                    }else {
+                        while (l<r&&nums[r]==nums[r-1]) r--;
+                        r--;
+                    }
+                }
+            }
+        }
+        return lists;
+    }
+}
